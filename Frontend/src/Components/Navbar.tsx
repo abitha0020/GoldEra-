0165fe693d44d108de97bb164088025a21b87f73
albinsajijohn <albinsajijohn@gmail.com>
2025-02-13 21:24:13 +0530

    Update Navbar.tsx

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
-
 const Navbar: React.FC = () => {
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav
       className="w-full h-16 flex items-center justify-between px-4"
       style={{ backgroundColor: '#C07F00' }}
     >
-     <h1  className="text-4xl font-bold text-white">GOLDERA </h1>
-      <ul className="flex space-x-8 text-black">
-        <li className="hover:text-gray-700 cursor-pointer">Home</li>
-        <li className="hover:text-gray-700 cursor-pointer">About Us</li>
-        <li className="hover:text-gray-700 cursor-pointer">Login</li>
-        <li>
+      <h1 className="text-4xl font-bold text-white">GOLDERA</h1>
+      <ul className="flex space-x-8 text-white text-lg">
+        <li className="hover:text-gray-700 cursor-pointer">
+          <a href="/">Home</a>
+        </li>
+        <li className="hover:text-gray-700 cursor-pointer" onClick={() => scrollToSection("about")}>
+          About Us
         </li>
       </ul>
     </nav>
