2b4328da780983a447d920906ae8978bece01f45
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2025-05-05 18:43:33 +0530

    Update NavbarLayout.tsx

--- Frontend/src/Routes/NavbarLayout.tsx
+++ Frontend/src/Routes/NavbarLayout.tsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 const NavbarLayout = () => {
     return (
         <div>
-            <Header name="ABC Jewellery"/>
+            <Header name="ABC Jewellery" homeLink="/JewelleryHome"/>
             <Outlet /> 
         </div>
     );
